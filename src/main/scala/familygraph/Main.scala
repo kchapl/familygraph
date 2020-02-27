6ff5584998cb447380b82ce5553c69113c0e3524
kchapl <kchapl@users.noreply.github.com>
2020-02-27 21:38:04 +0000

    Add menu

--- src/main/scala/familygraph/Main.scala
+++ src/main/scala/familygraph/Main.scala
@@ -5,25 +5,33 @@ import zio.{RIO, ZEnv, ZIO, console}
 
 object Main extends zio.App {
 
-  def getPerson(prompt: String): RIO[Console with Graph, Person] =
+  // TODO: only add if doesn't already exist by name and birth year
+  def addPerson(): RIO[Console with Graph, Unit] =
     for {
-      _         <- console.putStrLn(prompt)
+      _         <- console.putStrLn("Name:")
       name      <- console.getStrLn
+      _         <- console.putStrLn("Year of birth:")
       birthYear <- console.getStrLn
       _         <- Graph.>.add(Person(name, birthYear.toInt))
       person    <- Graph.>.getByName(name)
-      _         <- console.putStrLn(person.toString)
-    } yield person
-
-  val program: RIO[Console with Graph, Unit] =
-    for {
-      father <- getPerson("F")
-      mother <- getPerson("M")
-      child  <- getPerson("C")
-      _      <- Graph.>.addChildRelation(father, child)
-      _      <- Graph.>.addFatherRelation(child, father)
-      _      <- Graph.>.addMotherRelation(child, mother)
+      _         <- console.putStrLn(s"Added ${person.toString}")
     } yield ()
+
+  val program: RIO[Console with Graph, Unit] = {
+    def go(): RIO[Console with Graph, Unit] =
+      for {
+        _ <- console.putStrLn(
+          "Option:\n1. Add person\n2. Add mother-child relation\n3. Add father-child relation\n4. Exit")
+        option <- console.getStrLn
+        _ <- option match {
+          case "1" => addPerson() *> go()
+          case "2" => console.putStrLn("Option " + option) *> go()
+          case "3" => console.putStrLn("Option " + option) *> go()
+          case "4" => console.putStrLn("Option " + option)
+        }
+      } yield ()
+    go()
+  }
 
   def run(args: List[String]): ZIO[ZEnv, Nothing, Int] =
     program
